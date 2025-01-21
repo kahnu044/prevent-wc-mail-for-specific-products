ccb8739e6213c664f1d0592db311974c997e5530
kahnu044 <kahnucharan044@gmail.com>
2025-01-21 18:33:51 +0530

    pwcemail_admin_menu added

--- prevent-wc-email-for-products.php
+++ prevent-wc-email-for-products.php
@@ -11,3 +11,16 @@
 if (! defined('ABSPATH')) {
     exit;
 }
+
+// Add an admin menu to configure the plugin settings
+add_action('admin_menu', 'pwcemail_admin_menu');
+function pwcemail_admin_menu()
+{
+    add_menu_page('Prevent WC Email Settings', 'Prevent WC Email', 'manage_options', 'prevent-wc-email-settings', 'pwcemail_settings_page', 'dashicons-email-alt');
+}
+
+
+function pwcemail_settings_page()
+{
+    echo "Prevent WC Email Settings Page";
+}
