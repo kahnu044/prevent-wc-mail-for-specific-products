1acf1ad11f2d44b51d80b3e186d3bbb627453af2
kahnu044 <kahnucharan044@gmail.com>
2025-01-21 18:28:55 +0530

    Initial Plugin Added

---
+++ README.md
@@ -0,0 +1,2 @@
+# prevent-wc-mail-for-specific-products
+Prevent WooCommerce email notifications for specific products or categories
---
+++ prevent-wc-email-for-products.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Prevent WooCommerce Email for Specific Products
+ * Description: Prevent WooCommerce email notifications for specific products or categories.
+ * Version: 1.0.0
+ * Author: kahnu044
+ */
+
+// Exit if accessed directly
+if (! defined('ABSPATH')) {
+    exit;
+}
